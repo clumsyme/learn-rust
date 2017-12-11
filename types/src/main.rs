0c8abe2658be7a581ede5717356afb69d0fab9cf
LiYan <lidoyan@gmail.com>
2017-12-11 11:58:46 +0800

    types

--- types/src/main.rs
+++ types/src/main.rs
@@ -1,10 +1,23 @@
 fn main() {
-    let t = "21".parse().expect("what is this");
-    println!("Hello, world!");
+    let t: i32 = "21".parse().expect("what is this");
+    println!("Hello, {}!", t);
 
-    //     2 |     let t = "21".parse().expect("what is this");
-    //   |         ^
-    //   |         |
-    //   |         cannot infer type for `_`
-    //   |         consider giving `t` a type
+    let x = 57u8;
+    println!("{}", x);
+
+    let f = 3.14;
+    let f2: f32 = 3.1415;
+    println!("f: {}, f2: {}", f, f2);
+
+    // operate
+    let added = f2 + f;
+    println!("added: {}", added);
+
+    let tup: (i32, i32, f32) = (1, 222, 3.14);
+    let (one, two, three) = tup;
+    let one_alt = tup.0;
+    println!("one: {}, one_alt: {}", one, one_alt);
+
+    let arr = [1, 3, 5, 6, 8];
+    println!("arr[0]: {}", arr[0]);
 }
